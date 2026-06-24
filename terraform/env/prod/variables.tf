1ad6e6758bc868ab72f481012133df0edc8d392f
NimishDas <84317014+Nimish999@users.noreply.github.com>
2026-06-24 16:48:44 +0530

    Change default AWS region to ap-south-1

--- terraform/env/prod/variables.tf
+++ terraform/env/prod/variables.tf
@@ -1,4 +1,4 @@
-variable "aws_region"          { type = string; default = "us-east-1" }
+variable "aws_region"          { type = string; default = "ap-south-1" }
 variable "domain_name"         { type = string }
 variable "acm_certificate_arn" { type = string }
 variable "alb_dns_name"        { type = string; default = "" }
