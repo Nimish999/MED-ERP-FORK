b286544021d00fa95b8a0db6d930373c69d7479e
NimishDas <84317014+Nimish999@users.noreply.github.com>
2026-06-25 15:04:27 +0530

    Update api_fqdn output to handle empty records

--- terraform/modules/route53/outputs.tf
+++ terraform/modules/route53/outputs.tf
@@ -1,2 +1,2 @@
 output "frontend_fqdn" { value = aws_route53_record.frontend.fqdn }
-output "api_fqdn"      { value = aws_route53_record.api.fqdn }
+output "api_fqdn"      { length(aws_route53_record.api) > 0 ? aws_route53_record.api[0].fqdn : null }
