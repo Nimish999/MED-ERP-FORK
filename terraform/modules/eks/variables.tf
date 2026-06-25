37adc4811939855a555f796ea95894e082bd1742
NimishDas <84317014+Nimish999@users.noreply.github.com>
2026-06-25 12:20:50 +0530

    Change default node instance type to c7i-flex.large

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -21,7 +21,7 @@ variable "private_subnet_ids" {
 
 variable "node_instance_types" {
   type    = list(string)
-  default = ["m5.large"]
+  default = ["c7i-flex.large"]
 }
 
 variable "capacity_type" {
